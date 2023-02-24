6e26f39545d1941d099946caab2ec6fc6b9991d9
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-02-24 10:53:46 +0100

    Tester upload

--- wolt.js
+++ wolt.js
@@ -1,35 +1,18 @@
-
 // Knappen - 0 - Green - 1 - White - 2 - Red - 3 - Babyblue
 var colors = ["#749850", "white", "red", "#0D96E1"];
 
 function changeColor(element, color) {
-    element.style.backgroundColor = color
+  element.style.backgroundColor = color;
 }
 
 // Kun text- 0 - Green - 1 - White - 2 - Red - 3 - Babyblue
 var colors = ["#749850", "white", "red", "#0D96E1"];
 
 function changeColor1(element, color) {
-    element.style.color = color
+  element.style.color = color;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let restaurant = "Eller se populære restauranter og butikker i Odense";
+{
+  document.getElementById("restauranter").innerHTML = restaurant;
+}
