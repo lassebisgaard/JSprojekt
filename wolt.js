f10225038c5ca17081893126644b488bef3febcc
Knudsen53 <frederikknudsen132@gmail.com>
2023-02-24 04:49:28 +0100

    næsten done

--- wolt.js
+++ wolt.js
@@ -1,10 +1,16 @@
 
-var colors = ["black", "white", "red", "green"];
+//0 - Green - 1 - White - 2 - Red - 3 - Babyblue
+var colors = ["#749850", "white", "red", "#0D96E1"];
 
 function changeColor(element, color) {
-    element.style.backgroundCollor = color
+    element.style.backgroundColor = color
 }
 
+var colors = ["#749850", "white", "red", "#0D96E1"];
+
+function changeColor(element, color) {
+    element.style.color = color
+}
 
 
 
