6bffbe6b64666d14c43d608c6712fe578ab28ca7
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-02-24 11:13:14 +0100

    Deletos

--- wolt.js
+++ wolt.js
@@ -11,8 +11,3 @@ var colors = ["#749850", "white", "red", "#0D96E1"];
 function changeColor1(element, color) {
   element.style.color = color;
 }
-
-let restaurant = "Eller se populære restauranter og butikker i Odense";
-{
-  document.getElementById("restauranter").innerHTML = restaurant;
-}
