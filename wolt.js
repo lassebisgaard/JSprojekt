2ee620529581816b9f4bdf1f3481d50b6c257eb0
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-02-26 12:14:35 +0100

    Slettet knap footer

--- wolt.js
+++ wolt.js
@@ -12,37 +12,6 @@ function changeColor1(element, color) {
   element.style.color = color;
 }
 
-//David
-const openModalButtons = document.querySelectorAll("[data-modal-target]");
-const closeModalButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById("overlay");
-
-openModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = document.querySelector(button.dataset.modalTarget);
-    openModal(modal);
-  });
-});
-
-closeModalButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = button.closest(".modal");
-    closeModal(modal);
-  });
-});
-
-function openModal(modal) {
-  if (modal == null) return;
-  modal.classlist.add("active");
-  overlay.classlist.add("active");
-}
-
-function closeModal(modal) {
-  if (modal == null) return;
-  modal.classlist.remove("active");
-  overlay.classlist.remove("active");
-}
-
 //johannes
 function detectMad() {
   var mad = document.getElementById("mad").value;
