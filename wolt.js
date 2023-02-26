c1c5d6f2b191cc61531568c24f2a547fac427358
Davidmms95 <121063495+Davidmms95@users.noreply.github.com>
2023-02-26 11:42:32 +0100

    Knap om wolt

--- wolt.js
+++ wolt.js
@@ -11,3 +11,33 @@ var colors = ["#749850", "white", "red", "#0D96E1"];
 function changeColor1(element, color) {
   element.style.color = color;
 }
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classlist.add("active");
+  overlay.classlist.add("active");
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classlist.remove("active");
+  overlay.classlist.remove("active");
+}
